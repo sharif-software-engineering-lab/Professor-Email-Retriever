8b86f10eda0fc698311535d0b7af700ab09e15f4
Parsa Mohammadian <parsa2820@gmail.com>
2023-10-27 19:06:09 +0330

    Add telegram bot base

--- src/main.py
+++ src/main.py
@@ -1,4 +1,26 @@
 from core.mapper import Mapper
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import logging
 
 
-print(Mapper.get("kharrazi"))
+TOKEN_FILE = "token"
+f = open(TOKEN_FILE)
+token = f.read()
+f.close()
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(token).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
