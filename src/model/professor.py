6e250f3ce50caf3bb1fa3590c1e09a3d14bf40dd
Parsa Mohammadian <parsa2820@gmail.com>
2023-10-27 20:39:58 +0330

    Fix pattern matching bug

--- src/model/professor.py
+++ src/model/professor.py
@@ -8,4 +8,4 @@ class Professor:
         self.email = email
 
     def match(self, message):
-        return bool(re.match(self.pattern, message))
+        return bool(re.findall(self.pattern, message))
